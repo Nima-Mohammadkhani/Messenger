f1ccdc46bd6e5d3e4926ac37f94e6be3e2059470
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-08-16 11:30:16 +0330

    add vite pages

--- MessengerWebApp/src/App.tsx
+++ MessengerWebApp/src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import routes from "~react-pages";
 import PWABadge from "./PWABadge.tsx";
+
+function AppRoutes() {
+  const element = useRoutes(routes);
+  return element;
+}
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <PWABadge />
-    </>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
