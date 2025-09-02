97356ecafdf83789c82fbdd73cd74849dc189ff3
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-09-02 21:26:27 +0330

    add nativewind for use tailwindCss

---
+++ MessengerApp/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const nativewind = require("nativewind/preset");
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [nativewind],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
