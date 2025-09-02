77079712c2a05d4d031b7133170777d6bfc02cf7
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-09-02 22:44:43 +0330

    update

---
+++ MessengerApp/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView, StatusBar } from "react-native";
+
+const Index = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar></StatusBar>
+    </SafeAreaView>
+  );
+};
+export default Index;
